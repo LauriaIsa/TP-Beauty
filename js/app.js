d7bda8806cd74ffbf4d8241c0a3234f9f975c757
Luka1204 <66627548+Luka1204@users.noreply.github.com>
2023-04-27 20:35:19 -0300

    Boton hacia arriba 27/04/2023 20:35PM

--- js/app.js
+++ js/app.js
@@ -15,3 +15,21 @@ productContainers.forEach((item, i) => { /* Por cada item encontrado en el conte
         item.scrollLeft -= 290;  /* Por cada click al boton el item se movera en -x segun en tamaño del contenedor */
     })
 })
+
+
+window.onscroll = ()=>{
+    console.log(document.documentElement.scrollTop)
+    if(document.documentElement.scrollTop > 100){ //Cuando el nivel de scroll sea mayor a 100
+        document.querySelectorAll('.go-top-container')[0].classList.add('show')
+        console.log()
+    }else{
+        document.querySelectorAll('.go-top-container')[0].classList.remove('show')
+    }
+}
+
+document.querySelectorAll('.go-top-container')[0].addEventListener('click',()=>{
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+})
